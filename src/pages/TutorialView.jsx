b8cdc7d83571a37cfa78d8e45a75ba3f684d965c
PastelPossum <iggyskywalker@gmail.com>
2023-05-30 08:49:09 +0200

    Add dummy code to front page - button header and p

--- src/pages/TutorialView.jsx
+++ src/pages/TutorialView.jsx
@@ -2,21 +2,42 @@ import axios from "axios";
 import { useLoaderData } from "react-router-dom";
 import styled from "styled-components";
 
-const StyledButton = styled.button `
-background-color: #57419D;
-border-radius: 32px;
-border: none;
-color: white;
-width: 327px;
-height: 44px;
-font-size: 16px;
+const StyledHeader = styled.h1`
+  font-size: 20px;
+  font-weight: bold;
+  align-items: center;
+  text-align: center;
+`;
+
+const StyledP = styled.p`
+  font-size: 16px;
+  color: #9E9E9E;
+  align-items: center;
+  text-align: center;
+`;
+
+const StyledButton = styled.button`
+  background-color: #57419d;
+  border-radius: 32px;
+  border: none;
+  color: white;
+  width: 327px;
+  height: 44px;
+  font-size: 16px;
 `;
 
 const TutorialView = () => {
-   const TutorialViewData = useLoaderData();
-    return ( <>
-    <StyledButton>skip</StyledButton>
-    </> );
-}
- 
+  const TutorialViewData = useLoaderData();
+  return (
+    <>
+      <StyledHeader>My Pets</StyledHeader>
+      <StyledP>
+        Taking care of a pet is my favorite, it helps me to gaimr stress and
+        fatigue.
+      </StyledP>
+      <StyledButton>skip</StyledButton>
+    </>
+  );
+};
+
 export default TutorialView;
