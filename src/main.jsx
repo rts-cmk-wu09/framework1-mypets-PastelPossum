d57400257a62bef4011ffb76336ab44658e9901d
PastelPossum <iggyskywalker@gmail.com>
2023-05-30 08:47:19 +0200

    add route

---
+++ src/main.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import {
+  createBrowserRouter,
+  createRoutesFromElements,
+  Route,
+  RouterProvider,
+} from "react-router-dom";
+import App from "./App";
+import TutorialView from "./pages/TutorialView";
+
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route index element={<TutorialView />} />
+  )
+);
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <RouterProvider router={router} />
+  </React.StrictMode>
+);
