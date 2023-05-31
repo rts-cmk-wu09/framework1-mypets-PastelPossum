421f21c3df7c17b943edfad173b7420de9941e99
PastelPossum <iggyskywalker@gmail.com>
2023-05-31 10:07:43 +0200

    fixed position of button

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -8,10 +8,15 @@ const StyledButton = styled.button`
   width: 327px;
   height: 44px;
   font-size: 16px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  margin-left: 2%;
+  margin-top: 65%;
 `;
 
 const Button = (props) => {
-  return <StyledButton>{props.title}</StyledButton>;
+  return <StyledButton className="box-shadow">{props.title}</StyledButton>;
 };
 
 export default Button;
