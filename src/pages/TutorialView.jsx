1441c7f36e4e89f30fde7d81bb4b7dd484b5b0e0
PastelPossum <iggyskywalker@gmail.com>
2023-05-31 10:10:09 +0200

    fixed font and positioning of text

--- src/pages/TutorialView.jsx
+++ src/pages/TutorialView.jsx
@@ -3,9 +3,11 @@ import { useLoaderData } from "react-router-dom";
 import styled from "styled-components";
 import Button from "../components/Button";
 
+import AnimalList from "../templates/AnimalList";
+
 const StyledHeader = styled.h1`
   font-size: 20px;
-  font-weight: bold;
+  font-weight: bolder;
   align-items: center;
   text-align: center;
 `;
@@ -21,12 +23,17 @@ const TutorialView = () => {
   const TutorialViewData = useLoaderData();
   return (
     <>
+      
+      <img src="/src/assets/Animal.png" alt="" />
       <StyledHeader>My Pets</StyledHeader>
       <StyledP>
         Taking care of a pet is my favorite, it helps me to gaimr stress and
         fatigue.
       </StyledP>
       <Button title="Skip" />
+
+      {/* <AnimalList /> */}
+
     </>
   );
 };
